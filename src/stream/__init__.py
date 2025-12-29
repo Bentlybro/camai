a1892e9df95c3f8d8b960c82461a79e7ebee5686
Bentlybro <Github@bentlybro.com>
2025-12-29 11:30:11 +0000

    Update __init__.py

--- src/stream/__init__.py
+++ src/stream/__init__.py
@@ -1,9 +1,8 @@
 """Stream module - video streaming and annotation."""
 from .server import StreamServer
-from .annotate import annotate_frame, draw_skeletons
+from .annotate import annotate_frame
 
 __all__ = [
     "StreamServer",
     "annotate_frame",
-    "draw_skeletons",
 ]
